cd09b538650c616822d4e4f23a8476ca42706bc7
amandamolde <amanda.molde@gmail.com>
2018-08-23 20:41:00 -0600

    changed templates settings

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -142,12 +142,23 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATES = [
   {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [
-        os.path.join(BASE_DIR, 'build')
+      os.path.join(BASE_DIR, 'build')
     ],
-  }
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
+    },
+  },
 ]
 
 STATICFILES_DIRS = [
