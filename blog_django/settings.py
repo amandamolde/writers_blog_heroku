6146eae00933f6da8c64aa4cf98e02eb2add2262
amandamolde <amanda.molde@gmail.com>
2018-08-23 20:45:13 -0600

    Changed debug setting to false

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%w9u+295s-%n6zej-g-_nqr8v@+7tk#r6kqamupkofyer^hu1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['writers-blog-django.herokuapp.com', '127.0.0.1:8000']
 
